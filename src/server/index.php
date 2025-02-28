9f614881a56c5934b1c912074d48b1d2a2ea33ec
Robin Reyer <r.reyer@hs-mannheim.de>
2025-02-28 13:42:43 +0100

    add favicon

--- src/server/index.php
+++ src/server/index.php
@@ -6,8 +6,10 @@
 
 <!DOCTYPE html>
 <html lang="de">
-<link rel="icon" type="image/vnd.microsoft.icon" href="favicon.ico">
+	
   <head>
+  <link rel="icon" type="image/vnd.microsoft.icon" href="/var/www/gifybox/favicon.ico"/>
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
